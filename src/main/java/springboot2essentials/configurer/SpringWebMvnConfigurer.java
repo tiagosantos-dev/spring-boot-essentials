b8d973dbff7ed20276ef80ffd52a2f9fca1377a6
tiagosantos-dev <tiagoltj@hotmail.com>
2020-12-26 18:12:16 -0300

    Configurando a paginacao para 5(size) elementos e adicionando logging do hibernate

--- src/main/java/springboot2essentials/configurer/SpringWebMvnConfigurer.java
+++ src/main/java/springboot2essentials/configurer/SpringWebMvnConfigurer.java
@@ -1,4 +1,21 @@
 package springboot2essentials.configurer;
 
-public class SpringWebMvnConfigurer {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class SpringWebMvnConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
+
+        pageHandler.setFallbackPageable(PageRequest.of(0, 5));
+        resolvers.add(pageHandler);
+    }
 }
